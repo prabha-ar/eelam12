e8b4f5fc64928f0181ccc2544eec091b541bf239
wetarteam <78031947+wetarteam@users.noreply.github.com>
2021-07-08 02:04:18 +0530

    Changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as QuarklycommunityKitMobileSidePanel } from "./QuarklycommunityKitMobileSidePanel"
 export { default as QuarklycommunityKitAudio } from "./QuarklycommunityKitAudio"
+export { default as QuarklycommunityKitVideo } from "./QuarklycommunityKitVideo"
